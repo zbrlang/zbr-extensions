bad98afbb0fb2cee5d701d8cc48cc31d18373641
itszubariel <anishchapagain00@gmail.com>
2026-06-01 02:49:40 +0900

    fix tree-sitter grammar to split Z prefix and improve trigger lookahead logic

--- tree-sitter/grammar.js
+++ tree-sitter/grammar.js
@@ -1,6 +1,5 @@
 module.exports = grammar({
   name: "zbr",
-  externals: ($) => [$.function_start],
   extras: ($) => [$.comment],
   rules: {
     source_file: ($) =>
@@ -10,7 +9,6 @@ module.exports = grammar({
           $.function_call,
           $.trigger_call,
           $.text,
-          $.bare_z,
           $.newline,
         ),
       ),
@@ -20,7 +18,7 @@ module.exports = grammar({
         field(
           "key",
           alias(
-            token(prec(1, /\#(name|trigger|description|type|scope|option)[ \t]+/)),
+            token(prec(1, /#(name|trigger|description|type|scope|option)/)),
             $.header_name,
           ),
         ),
@@ -33,8 +31,9 @@ module.exports = grammar({
 
     function_call: ($) =>
       seq(
-        field("name", alias($.function_start, $.function_name)),
-        "{",
+        field("prefix", alias(token(prec(10, "Z")), $.function_prefix)),
+        field("name", alias(token(prec(10, /[a-zA-Z0-9_]+/)), $.function_name)),
+        token.immediate("{"),
         repeat(
           choice(
             $.function_call,
@@ -50,21 +49,43 @@ module.exports = grammar({
       ),
 
     trigger_call: ($) =>
-      seq(
-        field("name", $.trigger_name),
-        "{",
-        repeat(
-          choice(
-            $.function_call,
-            $.trigger_call,
-            $.braced_content,
-            $.escape_sequence,
-            $.operator,
-            $.argument_separator,
-            $.content,
+      choice(
+        seq(
+          field("name", alias("onInteraction", $.trigger_name)),
+          token.immediate("{"),
+          repeat(
+            choice(
+              $.function_call,
+              $.trigger_call,
+              $.braced_content,
+              $.escape_sequence,
+              $.operator,
+              $.argument_separator,
+              $.content,
+            ),
           ),
+          "}",
         ),
-        "}",
+        seq(
+          field("name", alias(/on[a-zA-Z0-9_]+/, $.trigger_name)),
+          optional(
+            seq(
+              token.immediate("{"),
+              repeat(
+                choice(
+                  $.function_call,
+                  $.trigger_call,
+                  $.braced_content,
+                  $.escape_sequence,
+                  $.operator,
+                  $.argument_separator,
+                  $.content,
+                ),
+              ),
+              "}",
+            )
+          )
+        )
       ),
 
     braced_content: ($) =>
@@ -84,18 +105,17 @@ module.exports = grammar({
         "}",
       ),
 
-    trigger_name: ($) => /on[a-zA-Z0-9_]+/,
     operator: ($) =>
       token(choice("==", "!=", ">=", "<=", ">", "<", "&&", "||")),
     argument_separator: ($) => token(";"),
     escape_sequence: ($) => token(/\\[{};\\]/),
     content: ($) => /[^Z;{}\\\n][^;{}\\\n]*/,
-    bare_z: ($) => token(prec(-1, "Z")),
     text: ($) =>
       token(
         choice(
           /[^#Z\n][^Z\n]*/,
-          /#[^(name|trigger|description|type|author)][^Z\n]*/,
+          /#(?!(name|trigger|description|type|scope|option))[^\n]*/,
+          "Z",
         ),
       ),
   },
--- tree-sitter/queries/highlights.scm
+++ tree-sitter/queries/highlights.scm
@@ -1,12 +1,34 @@
 ; Comments
 (comment) @comment
 
-; Functions
-"Z" @keyword
+; Headers
+(header_name) @keyword.control
+(header value: (_) @string)
+
+; Z functions
+(function_prefix) @keyword
 (function_name) @function
 "{" @punctuation.bracket
 "}" @punctuation.bracket
 
-; Headers
-"#" @punctuation.delimiter
-(header_name) @keyword
+; Trigger functions
+(trigger_name) @tag
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+
+; Braced content blocks
+(braced_content "{" @punctuation.bracket)
+(braced_content "}" @punctuation.bracket)
+
+; Argument separator
+(argument_separator) @keyword.control
+
+; Operators
+(operator) @operator
+
+; Escape sequences
+(escape_sequence) @string.escape
+
+; Content and text
+(content) @string
+(text) @string
